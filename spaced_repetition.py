da197f735c8cdbea2d89efe92f0059bd91cd79f0
blazakin <felschf05@gmail.com>
2024-08-17 23:00:38 -0700

    Added console logs

--- spaced_repetition.py
+++ spaced_repetition.py
@@ -10,9 +10,13 @@ socket.bind("tcp://*:5552")
 while True:
     message = socket.recv().decode()
 
+    print(f"Received: {message}")
     if message == 'Exit':
         break
 
     card_list = message.split(';')[:-1]
 
-    socket.send_string(card_list[random.randint(0, len(card_list)-1)])
+    chosen_card = card_list[random.randint(0, len(card_list)-1)]
+    socket.send_string(chosen_card)
+
+    print(f"Sent: {chosen_card}")
